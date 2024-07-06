fdbcef4701276a940a3a35cb313106a89626e63e
Rimuru <Rimuru.Dev@gmail.com>
2024-07-06 22:44:03 +0400

    Update index.js

--- index.js
+++ index.js
@@ -14,16 +14,9 @@ function generateProgressBar() {
 }
 
 const readme = `\
-### Hi there 👋
-⏳ Year progress ${progressBarOfThisYear} ${(progressOfThisYear * 100).toFixed(2)} %
+⏳ Time until vacation ${progressBarOfThisYear} ${(progressOfThisYear * 100).toFixed(2)} %
 ---
 ⏰ Updated on ${new Date().toUTCString()}
----
-
-[![Anurag's github stats](https://github-readme-stats-git-masterrstaa-rickstaa.vercel.app/api?username=RimuruDev&show_icons=true&count_private=true&custom_title=RimuruDev's%20GitHub%20profile&theme=vision-friendly-dark&line_height=40&hide_border=true)](https://github.com/anuraghazra/github-readme-stats)
-[![Anurag's github stats](https://github-readme-stats-git-masterrstaa-rickstaa.vercel.app/api/top-langs/?username=RimuruDev&custom_title=RimuruDev's%20code%20language&theme=radical&line_height=10hide_border=true&hide=html&exclude_repo=python_vim)](https://github.com/anuraghazra/github-readme-stats)
-
-![](https://raw.githubusercontent.com/RimuruDev/RimuruDev/master/assets/github-contribution-grid-snake.svg)
 \
 `
 console.log(readme)
